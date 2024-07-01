6797af0a654dbe73ab0cd7ec7bebf9dde65552c1
lianhong568 <90310275+lianhong568@users.noreply.github.com>
2024-07-02 00:21:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,21 @@
-/bin/bash #!/bin/bash
+#!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-#文件名：diy-part2.sh
-# 描述：OpenWrt DIY 脚本第二部分（更新后）
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# 版权所有 (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
-# 这是免费软件，根据 MIT 许可证授权。
-# 查看/LICENSE 以了解更多信息。
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
-#修改默认IP
-sed -i  's/192.168.1.1/192.168.2.1/g'包/基础文件/文件/bin/config_generate
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认主题
+# Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 修改主机名
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/P3TERX-Router/g' 包/基础文件/文件/bin/config_generate
